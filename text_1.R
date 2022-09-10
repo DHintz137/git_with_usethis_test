9b91b7947bd31cd1736da6dad69bb662815e2156
DHintz137 <dhintz1@uwyo.edu>
2022-09-10 17:56:36 -0600

    edit 3: removing "random text"

--- text_1.R
+++ text_1.R
@@ -2,6 +2,11 @@
 
 # edit 1
 
+
 # to revert back to an earlier version of a file you need the git id, known as a SHA
 
 # this can be found by navigating the tabs Git <- Diff <- History
+
+
+
+# random text 
